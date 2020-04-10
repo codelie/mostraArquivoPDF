7aa9c6450892e3a76c032f23bd8de8048f411e0a
codelie <62216257+codelie@users.noreply.github.com>
2020-04-10 15:17:03 -0300

    Add files via upload

---
+++ mostraArquivoPDF.js
@@ -0,0 +1,57 @@
+// Url do arquivo pdf
+var url = 'arquivoTeste.pdf';
+
+// Carrega via <script> tag, cria endereço para acesso ao PDF.js exports.
+var pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+// Local do dpf.worker.js
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'pdfjs-dist/build/pdf.worker.js';
+// Asynchronous download do PDF
+var loadingTask = pdfjsLib.getDocument(url);
+var pageNumber = 1;
+var texto ='';
+
+function renderizaPagina(pageNumber) {
+
+    loadingTask.promise.then(function(pdf) {
+        console.log('PDF loaded');
+       
+        // Pega primeira page
+        pdf.getPage(pageNumber).then(function(page) {
+        console.log('Page loaded: '+ pageNumber );
+        
+        var scale = 1.5;
+        var viewport = page.getViewport({scale: scale});
+
+        // Prepara canvas usando as dimensões da página PDF
+        var canvas = document.getElementById('the-canvas');
+        var context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        // Renderiza página PDF para canvas 
+        var renderContext = {
+        canvasContext: context,
+        viewport: viewport
+        };
+        var renderTask = page.render(renderContext);
+
+            renderTask.promise.then(function () {
+            console.log('Página renderizada!');
+        });
+    });
+    }, function (reason) {
+    // Mostra erro
+    console.error(reason);
+    });
+
+}
+
+pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
+    pdfDoc = pdfDoc_;
+    document.getElementById('page_count').textContent = pdfDoc.numPages;
+    document.getElementById('page_num').textContent = pageNumber;  
+ 
+    // Chamando a renderizacao da página
+    renderizaPagina(pageNumber);
+  });
